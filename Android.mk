a3f51f454e25569bc0b839eab9a04ed883c1bb48
Dyneteve <sound0020@gmail.com>
2018-03-27 05:49:02 -0400

    o7-common: Rebrand
    Change-Id: I3eef0208c5272468ec18d21e6b9852348f66f079

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter j5lte j5ltechn j5nlte j5nltexx j53gxx, $(TARGET_DEVICE)),)
+ifneq ($(filter o7prolte, $(TARGET_DEVICE)),)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
--- device-common.mk
+++ device-common.mk
@@ -19,9 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
 
 # Inherit from vendor
-$(call inherit-product-if-exists, vendor/samsung/j5-common/j5-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/o7-common/o7-common-vendor.mk)
 
-LOCAL_PATH := device/samsung/j5-common
+LOCAL_PATH := device/samsung/o7-common
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += \
