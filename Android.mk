15950ebbbe8f3965638421165a0ea033664b6ff1
Cyborg2017 <wangyang0987654@gmail.com>
2018-04-28 10:26:03 +0800

    o7-common: Add on7ltechn to barrier
    Change-Id: I65d37a9aee28fbc93fe35cd7a1ad70fa87cdc1a3

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter o7prolte, $(TARGET_DEVICE)),)
+ifneq ($(filter o7prolte on7ltechn, $(TARGET_DEVICE)),)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
